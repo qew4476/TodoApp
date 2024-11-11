0ce95d6b1788887cda3ec0cd591b1e3873f29f61
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2024-11-11 19:15:05 +0800

    response_class=HTMLResponse

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
-
+from starlette.responses import HTMLResponse
 
 from app.routes import routes, routes_auth
 
@@ -10,11 +10,16 @@ templates = Jinja2Templates(directory="templates")
 app.include_router(routes.task_router, prefix="/todolist", tags=["task_management"])
 app.include_router(routes_auth.auth_router, prefix="/auth", tags=["auth"])
 
-@app.get("/login",description="Login Page")
+@app.get("/login",description="Login Page", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/user/{user_id}")
+@app.get("/user/{user_id}", description="User Home Page", response_class=HTMLResponse)
 async def user_home(user_id: str,request:Request):
     # 這裡的 token 已經是經過驗證的 Bearer Token
     return templates.TemplateResponse("user_home.html", {"request": request, "user_id": user_id})
+
+#register the user
+@app.get("/register", description="Register Page", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
