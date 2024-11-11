70c192d9c4cdccffa2816b70b585258c218c186d
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2024-11-11 12:05:58 +0800

    home page(Api not yet implemented)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.templating import Jinja2Templates
 from app.routes import routes, routes_auth
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(routes.task_router, prefix="/todolist", tags=["task_management"])
 app.include_router(routes_auth.auth_router, prefix="/auth", tags=["auth"])
 
+@app.get("/login",description="Login Page")
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+#homw page
+@app.get("/",description="Home Page(To do list)")
+async def home_page(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
