65e8a3dc6aa570ac8040ba272e93a4ea98ff90dd
Shane-Yang <81016471+qew4476@users.noreply.github.com>
2024-11-11 18:52:22 +0800

    Update routes.py

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -7,7 +7,7 @@ from app.auth.jwt_bearer import JWTBearer
 
 task_router = APIRouter()
 
-@task_router.post('/task', response_model=pydantic_model.Task, dependencies=[Depends(JWTBearer()])
+@task_router.post('/task', response_model=pydantic_model.Task, dependencies=[Depends(JWTBearer())])
 def create_task(doc:pydantic_model.TaskCreate):
     return db.create_task(doc)
 
